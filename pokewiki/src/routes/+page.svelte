581092a41f6cdedd15d5e5fe9ffd85cbf9dde14d
murramge <dmdgi37@naver.com>
2024-08-02 16:41:35 +0900

    poke api 연결

--- pokewiki/src/routes/+page.svelte
+++ pokewiki/src/routes/+page.svelte
@@ -1,6 +1,14 @@
+<script>
+    import {arrPokemon} from "../stores/pokestore";
+
+</script>
+
+
 <svelte:head>
     <title>포켓몬 위키</title>
 </svelte:head>
 
 <h1 class="text-4xl text-center my-8 font-bold underline">포켓몬 위키 사이트</h1>
-
+{#each $arrPokemon as pokemon}
+<p>{pokemon.name}</p>
+{/each}
---
+++ pokewiki/src/stores/pokestore.js
@@ -0,0 +1,43 @@
+import { writable } from "svelte/store";
+
+export const arrPokemon = writable([]);
+
+const fetchNum = 150;
+
+const fetchPokemon = async () => {
+    const url = `https://pokeapi.co/api/v2/pokemon?limit=${fetchNum}`;
+    const res = await fetch(url);
+    const data = await res.json();
+
+    console.log(koreanNames)
+    const loadedPokemon = data.results.map((pokemon, index) => {
+        return {
+            name: koreanNames[index],
+            id: index + 1,
+            image: `https://raw.githubsercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`
+        }
+    })
+
+    arrPokemon.set(loadedPokemon);
+};
+
+
+let koreanNames = [];
+const urls = [];
+
+for (let i = 0; i < fetchNum; i++) {
+    let url = `https://pokeapi.co/api/v2/pokemon-species/${i + 1}`;
+    urls.push(url);
+}
+
+let requests = urls.map(url => fetch(url));
+
+Promise.all(requests)
+    .then((responses) => Promise.all(responses.map(res => res.json())))
+    .then((results) => {
+        for (let result of results) {
+            console.log(result)
+            koreanNames.push(result.names[2].name);
+        }
+        fetchPokemon();
+    });
